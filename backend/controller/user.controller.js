a59ef12c11b3bdf058a5692ebd33e31e8f41cd8f
dangvanty <dangvantydh1@gmail.com>
2022-09-28 17:52:34 +0700

    feat: sign_up backend

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,27 +1,26 @@
 const catchAsyncErrors = require('../middleware/catchAsyncError')
 const User = require('../models/user.model')
-const cloudinary =require("cloudinary")
-const sendToken=require("../utils/jwtToken");
+const cloudinary = require('cloudinary')
+const sendToken = require('../utils/jwtToken')
 //Register a User
-exports.registerUser=catchAsyncErrors( async(req,res,next)=>{
-    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
-      folder: "avatars",
-      width: 150,
-      crop: "scale",
-      public_id: `${Date.now()}`,
-      resource_type: "auto",
-    });
-      const { name, email, password }= req.body;
-      const user= await User.create({
-          name,
-          email,
-          password,
-          avatar:{
-              public_id:myCloud.public_id,
-              url:myCloud.secure_url,
-          },
-      })
-  
-      sendToken(user,201,res);
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    folder: 'avatars',
+    width: 150,
+    crop: 'scale',
+    public_id: `${Date.now()}`,
+    resource_type: 'auto',
   })
-  
+  const { name, email, password } = req.body
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
+    },
+  })
+
+  sendToken(user, 201, res)
+})
